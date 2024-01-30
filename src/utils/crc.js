1348e5affce78c90f061364bbcebf800805dec53
= <=>
2024-01-30 15:24:43 +0500

    calcCRC8 returns in decimal

--- src/utils/crc.js
+++ src/utils/crc.js
@@ -1,18 +1,17 @@
 const crc16modbus = require('crc/calculators/crc16modbus');
-const {crc8} = require('crc')
+const { crc8 } = require('crc');
 
-const data3 = [80, 49, 2, 40, 55, 55, 55, 55, 55, 55, 41, 3]
+const data3 = [80, 49, 2, 40, 55, 55, 55, 55, 55, 55, 41, 3];
 
-const CRC = (crc8(new Int8Array(data3)))
-console.log(CRC)
-
+const CRC = crc8(new Int8Array(data3));
+console.log(CRC);
 
 // Mercury, ...
 function CRC16Modbus(params) {
-    const CRC = String(crc16modbus(new Int8Array(params)).toString(16))
-    let crc1 = parseInt((CRC[2]+CRC[3]), 16)
-    let crc2 = parseInt((CRC[0]+CRC[1]), 16)
-    return {crc1, crc2}
+    const CRC = String(crc16modbus(new Int8Array(params)).toString(16));
+    let crc1 = parseInt(CRC[2] + CRC[3], 16);
+    let crc2 = parseInt(CRC[0] + CRC[1], 16);
+    return { crc1, crc2 };
 }
 
 // energomera, ...
@@ -22,10 +21,10 @@ function calcCRC8(hexString) {
     const last7Bits = paddedBinaryString.slice(-7);
     const hexResult = parseInt(last7Bits, 2).toString(16).toUpperCase();
 
-    return hexResult;
+    return parseInt(hexResult, 16);
 }
-
 
 module.exports = {
-    CRC16Modbus
-}
+    CRC16Modbus,
+    calcCRC8,
+};
