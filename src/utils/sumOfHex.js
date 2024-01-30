65ceefd0e1446d773442cd9e810dfa73545d3d89
= <=>
2024-01-30 14:42:46 +0500

    CRC creater added

--- src/utils/sumOfHex.js
+++ src/utils/sumOfHex.js
@@ -1,31 +1,57 @@
-function sumHexArray(hexArray) {
-    let data = verificationArray(hexArray)
-        const sumDecimal = data.reduce((acc, hexNumber) => {
-            const decimalValue = parseInt(hexNumber, 16);
-            if (isNaN(decimalValue)) {
-                throw new Error('Invalid hex input in the array');
-            }
-            return acc + decimalValue;
-        }, 0);
-        return sumDecimal.toString(16).toUpperCase();
+function CRC8(hexArray) {
+    let data = verificationArray(hexArray);
+    const sumDecimal = data.reduce((acc, hexNumber) => {
+        const decimalValue = parseInt(hexNumber, 16);
+        if (isNaN(decimalValue)) {
+            throw new Error('Invalid hex input in the array');
+        }
+        return acc + decimalValue;
+    }, 0);
+
+    return calcCRC(sumDecimal.toString(16).toUpperCase());
 }
+let x = CRC8([
+    '01',
+    '50',
+    '30',
+    '02',
+    '28',
+    '31',
+    '44',
+    '34',
+    '36',
+    '30',
+    '43',
+    '37',
+    '35',
+    '29',
+    '03',
+]);
+console.log(x);
 
-// console.log(sumHexArray(['01', '50', '31', '02', '28', '37', '37', '37', '37', '37', '37', '29', '03']))
+module.exports = CRC8;
 
-module.exports = sumHexArray;
-
-function verificationArray (data) {
-    let soh = '01'
-    let stx = '02'
-    let etx = '03'
+function verificationArray(data) {
+    let soh = '01';
+    let stx = '02';
+    let etx = '03';
     if (data[0] === soh || data[0] === stx) {
-        data.shift()
+        data.shift();
     }
-    let x
-    for(let i in data) {
+    let x;
+    for (let i in data) {
         if (data[i] === '03') {
-            x = i
+            x = i;
         }
     }
-    return data.splice(0, +x+1)
+    return data.splice(0, +x + 1);
+}
+
+function calcCRC(hexString) {
+    const binaryString = parseInt(hexString, 16).toString(2);
+    const paddedBinaryString = binaryString.padStart(hexString.length * 4, '0');
+    const last7Bits = paddedBinaryString.slice(-7);
+    const hexResult = parseInt(last7Bits, 2).toString(16).toUpperCase();
+
+    return hexResult;
 }
