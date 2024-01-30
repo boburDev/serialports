a72a41cba66b635498295c345a2f38f2a0aeae4d
boburDev <negmatov1907@gmail.com>
2024-01-30 12:13:56 +0500

    Create convertor.js

---
+++ src/utils/convertor.js
@@ -0,0 +1,23 @@
+function decimalToHex(d, padding) {
+    if (Array.isArray(d)) {
+        return d.map(value => {
+            var hex = Number(value).toString(16);
+            padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+            while (hex.length < padding) {
+                hex = "0" + hex;
+            }
+            return hex
+        })
+    } else {
+        var hex = Number(d).toString(16);
+        padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+        while (hex.length < padding) {
+            hex = "0" + hex;
+        }
+        return hex
+    }
+}
+
+module.exports = decimalToHex
