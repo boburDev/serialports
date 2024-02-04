52c9a691dbe41c5c6b0e8fa7674cf86067368781
boburDev <negmatov1907@gmail.com>
2024-02-04 16:15:07 +0500

    query maker updated

--- src/express/controller/counter_connection.js
+++ src/express/controller/counter_connection.js
@@ -7,44 +7,45 @@ const { makeQuery } = require('../../utils/makeQuery.js');
 
 module.exports = {
     GET: async (req, res) => {
-        let { Reading: data } = { Reading: ['0.0', '0.1', '0.2', '1.4'] };
+        let { Reading: data } = { Reading: ['0.0', '0.1', '0.2', '1.4', '1.0.0'] };
         let queries = makeQuery(data);
+        console.log(queries)
 
         // console.log(q)
         // console.log(Object.keys(q))
         // console.loq(req.body)
-        const requests = [
-            { version: queries[0]['data'], crc: false },
-            { hashedPassword: queries[1]['data'], crc: false },
-            { password: queries[2]['data'] },
-            { volta: queries[3]['data'] },
-            // { 'voltl': q.currentData.voltl },
-            // { 'frequency': q.currentData.frequency },
-            // { 'current': q.currentData.current },
-            // { 'powp': q.currentData.powp },
-            // { 'powq': q.currentData.powq },
-            // { 'pows': q.currentData.pows },
-            // { 'coriu': q.currentData.coriu },
-            // { 'coruu': q.currentData.coruu },
-            // { 'cosf': q.currentData.cosf },
-            // { 'tanf': q.currentData.tanf },
-            // { 'currentDate': q.currentDate },
-            // { 'closeCommand': q.closeCommand },
-        ];
+        // const requests = [
+        //     { version: queries[0]['data'], crc: false },
+        //     { hashedPassword: queries[1]['data'], crc: false },
+        //     { password: queries[2]['data'] },
+        //     { volta: queries[3]['data'] },
+        //     // { 'voltl': q.currentData.voltl },
+        //     // { 'frequency': q.currentData.frequency },
+        //     // { 'current': q.currentData.current },
+        //     // { 'powp': q.currentData.powp },
+        //     // { 'powq': q.currentData.powq },
+        //     // { 'pows': q.currentData.pows },
+        //     // { 'coriu': q.currentData.coriu },
+        //     // { 'coruu': q.currentData.coruu },
+        //     // { 'cosf': q.currentData.cosf },
+        //     // { 'tanf': q.currentData.tanf },
+        //     // { 'currentDate': q.currentDate },
+        //     // { 'closeCommand': q.closeCommand },
+        // ];
 
-        let result = [];
-        await openPort();
-        for (let request of requests) {
-            let data = await getData(request);
-            console.log(data);
-            if (data) {
-                result.push({ [Object.keys(request)[0]]: data });
-            }
-        }
+        // let result = [];
+        // await openPort();
+        // for (let request of requests) {
+        //     let data = await getData(request);
+        //     console.log(data);
+        //     if (data) {
+        //         result.push({ [Object.keys(request)[0]]: data });
+        //     }
+        // }
 
-        await getData(requests[requests.length - 1]);
-        await closePort();
-        res.json({ data: result, status: 200 });
+        // await getData(requests[requests.length - 1]);
+        // await closePort();
+        res.json({ data: "result", status: 200 });
         // res.json({ data: 'success', status: 200 })
     },
 };
--- src/utils/makeQuery.js
+++ src/utils/makeQuery.js
@@ -7,9 +7,9 @@ function makeQuery(data) {
         const requestData = getRequest(val);
         return [
             ...result,
-            { data: requestData },
+            requestData,
             ...makeQuery(val.children || []),
-        ];
+            ];
     }, []);
 }
 
@@ -17,15 +17,38 @@ function getRequest(argument) {
     const q = queries[0];
     let dataR = {};
     let dataY = '';
-    for (let i of argument.split('.')) {
-        if (dataY.length > 0) {
-            dataY = Object.keys(dataR)[+i];
-            dataR = dataR[dataY];
-        } else {
-            let index = Object.keys(q)[+i];
-            dataR = q[index];
-            dataY = index;
+    let data = argument.split('.')
+    let dataKey = ''
+    if (data.length > 2) {  
+        for (let i in data) {
+            if (dataY.length > 0) {
+                dataY = Object.keys(dataR)[+data[i]];
+                dataR = dataR[dataY];
+                if (i >= 1) {
+                    if (dataKey.length) {
+                        dataKey += `.${dataY}`
+                    } else {
+                        dataKey += dataY
+                    }
+                }
+            } else {
+                let index = Object.keys(q)[+data[i]];
+                dataR = q[index];
+                dataY = index;
+            }
+        }
+    } else {
+        for (let i of data) {
+            if (dataY.length > 0) {
+                dataY = Object.keys(dataR)[+i];
+                dataR = dataR[dataY];
+                dataKey = dataY 
+            } else {
+                let index = Object.keys(q)[+i];
+                dataR = q[index];
+                dataY = index;
+            }
         }
     }
-    return dataR;
+    return {[dataKey]: dataR};
 }
