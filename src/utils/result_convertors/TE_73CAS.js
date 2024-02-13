962c468a1f9a421e11d283944bd839bbd694f1f4
= <=>
2024-02-13 11:48:47 +0500

    result maker changed

--- src/utils/result_convertors/TE_73CAS.js
+++ src/utils/result_convertors/TE_73CAS.js
@@ -1,13 +1,15 @@
 module.exports = { getTE_73Result };
 
-
 function getTE_73Result(data, key) {
     try {
+        console.log(data);
         const hexString = data.toString('hex');
-        let dataBufArray = hexString.split(hexString.split('').slice(0, 26).join(''))[1].match(/.{1,2}/g).slice(0, -3);
-        
-        let pad2 = n => String(n).padStart(2, '0')
-        
+        let dataBufArray = hexString
+            .split(hexString.split('').slice(0, 26).join(''))[1]
+            .match(/.{1,2}/g)
+            .slice(0, -3);
+        let pad2 = n => String(n).padStart(2, '0');
+        const result = dataBufArray.join('').slice(-8);
         if (key == 'currentDate') {
             let year = parseInt(dataBufArray[7] + dataBufArray[8], 16);
             let month = parseInt(dataBufArray[9], 16);
@@ -15,14 +17,15 @@ function getTE_73Result(data, key) {
             let hour = parseInt(dataBufArray[12], 16);
             let minute = parseInt(dataBufArray[13], 16);
             let second = parseInt(dataBufArray[14], 16);
-            const result = `${pad2(day)}-${pad2(month)}-${year} ${pad2(hour)}:${pad2(minute)}:${pad2(second)}`;
-            return { [key]: result }
+            const result = `${pad2(day)}-${pad2(month)}-${year} ${pad2(
+                hour
+            )}:${pad2(minute)}:${pad2(second)}`;
+            return { [key]: result };
         } else if (key === 'frequency') {
-            const result = hexString.slice(-10, -6);
-            const frequency = parseInt(result, 16) / 100;
-            return { [key]: frequency }
+            const frequency = parseInt(result.slice('-4'), 16) / 100;
+            return { [key]: frequency };
         } else {
-            console.log(data);   
+            console.log(data);
         }
     } catch (error) {
         throw new Error(error.message);
