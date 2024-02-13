10f54f58fe80c145f824a54a497b6cd9a919ed39
boburDev <65768571+boburDev@users.noreply.github.com>
2024-02-13 13:07:33 +0500

    ok

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -4,12 +4,12 @@ module.exports.Validation = Joi.object().keys({
     MeterType: Joi.string().required(),
     MeterAddress: Joi.string(),
     MeterPassword: Joi.string().required(),
-    commMedia: Joi.string().required(),
+    commMedia: Joi.number().required(),
     commDetail1: Joi.string().required(),
     commDetail2: Joi.number().required(),
-    parity: Joi.string().required(),
-    stopBit: Joi.number().required(),
-    dataBit: Joi.number().required(),
+    parity: Joi.string(),
+    stopBit: Joi.number(),
+    dataBit: Joi.number(),
     ReadingRegister: Joi.array().items(Joi.string()).required(),
     ReadingRegisterTime: Joi.array().items(Joi.number().required()).min(6).max(6)
 })
