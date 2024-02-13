05ddd3ee41a7c720095d0f550ee489da666148f1
boburDev <65768571+boburDev@users.noreply.github.com>
2024-02-13 13:08:28 +0500

    Update counter_connection.js

--- src/express/controller/counter_connection.js
+++ src/express/controller/counter_connection.js
@@ -4,13 +4,11 @@ module.exports = {
     POST: async (req, res) => {
         try {
             const { error, value } = Validation.validate(req.body)
-            console.log(error, value);
-            
             if (error) throw new Error(error.message)
 
-            // const lstData = value.ReadingRegisterTime
-            // const data = lstData ? await getLstCounterResult(value) : await getCounterResult(value)
-            res.json({result: 'data', error: false, status: 200})
+            const lstData = value.ReadingRegisterTime
+            const data = lstData ? await getLstCounterResult(value) : await getCounterResult(value)
+            res.json({result: data, error: false, status: 200})
         } catch (error) {
             res.json({data: null, error: error.message, status: 400})
         }
