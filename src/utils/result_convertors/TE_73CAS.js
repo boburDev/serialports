6d6eb4b26d31f72d16c0f740a98dff8c9b6c6fcb
boburDev <65768571+boburDev@users.noreply.github.com>
2024-02-13 12:03:08 +0500

    Update TE_73CAS.js

--- src/utils/result_convertors/TE_73CAS.js
+++ src/utils/result_convertors/TE_73CAS.js
@@ -1,29 +1,23 @@
 module.exports = { getTE_73Result };
 
+
 function getTE_73Result(data, key) {
     try {
-        console.log(data);
         const hexString = data.toString('hex');
-        let dataBufArray = hexString.split(hexString.split('').slice(0, 26).join(''))[1].match(/.{1,2}/g).slice(0, -3);
-        
-        let pad2 = n => String(n).padStart(2, '0')
-
-        if (key == 'currentDate') {
-            let year = parseInt(dataBufArray[7] + dataBufArray[8], 16);
-            let month = parseInt(dataBufArray[9], 16);
-            let day = parseInt(dataBufArray[10], 16);
-            let hour = parseInt(dataBufArray[12], 16);
-            let minute = parseInt(dataBufArray[13], 16);
-            let second = parseInt(dataBufArray[14], 16);
-            const result = `${pad2(day)}-${pad2(month)}-${year} ${pad2(
-                hour
-            )}:${pad2(minute)}:${pad2(second)}`;
-            return { [key]: result };
-        } else if (key === 'frequency') {
-            const frequency = parseInt(result.slice('-4'), 16) / 100;
-            return { [key]: frequency };
-        } else {
-            console.log(key, data, dataBufArray);   
+        let dataBufArray = hexString.split(hexString.slice(0, 26))[1].match(/.{1,2}/g).slice(0, -3)
+        let currentVal = dataBufArray.join('').slice(-8)
+        switch (key) {
+            case 'currentDate':
+                return { [key]: currentDate(dataBufArray) };
+            case 'frequency':
+                const frequency = parseInt(currentVal.slice(-4), 16) / 100;
+                return { [key]: frequency };
+            case '':
+                return 'ok'
+            default:
+                const res = parseInt(currentVal.slice(-4), 16) / 100;
+                console.log(key, currentVal, res);
+                return { [key]: 'data' };
         }
     } catch (error) {
         throw new Error(error.message);
@@ -31,5 +25,15 @@ function getTE_73Result(data, key) {
 }
 
 function currentDate(params) {
-    
+    const year = parseInt(params.slice(7, 9).join(''), 16);
+    const month = parseInt(params[9], 16);
+    const day = parseInt(params[10], 16);
+    const hour = parseInt(params[12], 16);
+    const minute = parseInt(params[13], 16);
+    const second = parseInt(params[14], 16);
+    return `${pad2(day)}-${pad2(month)}-${year} ${pad2(hour)}:${pad2(minute)}:${pad2(second)}`;
+}
+
+function pad2(n) {
+    return String(n).padStart(2, '0');
 }
